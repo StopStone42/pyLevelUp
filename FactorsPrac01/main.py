4922beda653d6eafed4c07c58f52a37b058aecbb
StopStone42 <77120604+StopStone42@users.noreply.github.com>
2021-01-29 16:21:18 +0900

    FactorsPrac01

---
+++ FactorsPrac01/main.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = 0
+    i = 1
+    for i in range(i,n+1):
+        if n % i == 0:
+            answer += i
+    return answer
+
+
+if __name__ == '__main__':
+    print(solution(12))
