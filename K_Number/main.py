f4317ab413c912b818dacf6315103424a7d71255
StopStone42 <77120604+StopStone42@users.noreply.github.com>
2021-02-19 19:30:37 +0900

    K Number

---
+++ K_Number/main.py
@@ -0,0 +1,14 @@
+def solution(array, commands):
+    answer = []
+
+    for command in commands:
+        start,end,s = command[0], command[1], command[2]
+
+        temp = array[start - 1:end]
+        temp.sort()
+        answer.append(temp[s-1])
+
+    return answer
+if __name__ == '__main__':
+    print(solution([1, 5, 2, 6, 3, 7, 4],
+                   [[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
