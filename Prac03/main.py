ad85b63eac83ab2aa801c86d5d3df01600f08a98
StopStone42 <77120604+StopStone42@users.noreply.github.com>
2021-03-07 20:19:19 +0900

    Prac03

---
+++ Prac03/main.py
@@ -0,0 +1,16 @@
+def solution(s):
+    stack = []
+
+    for i in s:
+        if i == '(':
+            stack.append(i)
+        else:
+            if len(stack) == 0:
+                return False
+            else:
+                stack.pop()
+    return len(stack) == 0
+
+
+if __name__ == '__main__':
+    print(solution("((())"))
