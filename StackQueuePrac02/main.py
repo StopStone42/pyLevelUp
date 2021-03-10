8e21fd63e12abc60df28625b4d796b06da9da474
StopStone42 <77120604+StopStone42@users.noreply.github.com>
2021-03-10 20:19:19 +0900

    StackQueuePrac02

---
+++ StackQueuePrac02/main.py
@@ -0,0 +1,22 @@
+def solution(progresses, speeds):
+    answer = []
+    time = 0
+    count = 0
+
+    while len(progresses):
+        if progresses[0] + speeds[0] * time >= 100:
+            progresses.pop(0)
+            speeds.pop(0)
+            count += 1
+
+        else:
+            if count > 0:
+                answer.append(count)
+                count = 0
+            time += 1
+    answer.append(count)
+    return answer
+
+
+if __name__ == '__main__':
+    print(solution([93, 30, 55], [1, 30, 5]))
