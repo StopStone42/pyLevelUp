c13450a7dab3b1d509be7125ad03dd25b49d2020
StopStone42 <77120604+StopStone42@users.noreply.github.com>
2021-03-21 20:19:19 +0900

    Prac08

---
+++ Prac08/main.py
@@ -0,0 +1,7 @@
+def solution(A,B):
+    A = sorted(A)
+    B = sorted(B, reverse=True)
+    return sum(a*b for a, b in zip(A, B))
+
+if __name__ == '__main__':
+    print(solution([1,4,2], [5,4,4]))
